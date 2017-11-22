495c7cb38a66a854d9111d675f0d6e496fc1fade
Extazx2 <touin.marcantoine@gmail.com>
2017-11-22 15:29:21 +0100

    mise a jour 2 22/11

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
 document.addEventListener("DOMContentLoaded", function(event) {
 	console.log("toto");
-		//document.querySelectorAll('section').display = "none";
+	//document.querySelectorAll('section').display = "none";
+	document.getElementById("projets").style.display = "none";
+	document.getElementById("skills").style.display = "none";
+	document.getElementById("experience").style.display = "none";
+	document.getElementById("contact").style.display = "none";
 });
