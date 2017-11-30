111e19351a0f1f39020d96b26296844b7e42aea6
Extazx2 <touin.marcantoine@gmail.com>
2017-11-30 01:09:01 +0100

    last maj of the day
    going to sleep, cy@ soon

---
+++ index.php
@@ -0,0 +1,112 @@
+<!DOCTYPE html>
+<html lang="fr" class="no-js">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+	<title>Marc-Antoine Touin</title>
+
+	<meta name="description" content="Portfolio numérique de Marc-Antoine Touin, Étudiant Ingénieur." />
+	<meta name="keywords" content="isen, brest, etudiant, etudes, ingenieur, cir, digital, design, rennes, touin, marc-antoine, marc, antoine, web, css, html" />
+	<meta name="author" content="Marc-Antoine Touin" />
+
+	<link rel="shortcut icon" href="img/favicon.png">
+
+	<!--			CSS 				-->
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+	<link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<script src="js/script.js"></script>
+	<script src="js/jquery-3.2.1.min.js"></script>
+
+
+</head>
+<body>
+	<header>
+		<nav id="menu">
+			<div class="nav-wrapper">
+				<a class="logo">MARC-ANTOINE TOUIN</a>
+				<ul id="nav-mobile" class="right hide-on-med-and-down">
+					<li><button type="button" class="menu-btn" data-name="index" target="index">Accueil</button></li>
+					<li><button type="button" class="menu-btn" data-name="experience" target="experience">Expériences</button></li>
+				</ul>
+			</div>
+		</nav>
+
+	</header>
+	<main>
+		<div id="main-content" class="row">
+			<div id="wip" class="col s12 valign-wrapper">
+				<h2 class="center-align truncate">SITE EN COURS DE CONSTRUCTION</h2>
+			</div>
+			<div id="aboutme" class="col s12">
+				<h2 class="underlined"><span class="part1">PRÉSEN</span>TATION</h2>
+				<p>Je suis Marc-Antoine Touin, étudiant-ingénieur de 21 ans en 4è année du cursus d'ingénieur à <button class="link" href="http://isen-brest.fr/" target="_blank">YNCREA Ouest - ISEN Brest</button> en option "Génie Logiciel".</p>
+				<p>J'ai passé les 3 premières années de ce cursus au sein de la formation "Cycle Informatique et Réseaux", c'est un cycle dérivé des classes préparatoires classiques comportant moins de matières "scientifiques", remplacées par de l'informatique.</p>
+				<p>Je suis actuellement à la recherche d'une alternance pour l'année 2017-2018, étant originaire de La Guerche de Bretagne, en Ille-et-Vilaine, je suis disponible à Rennes comme à Brest.</p>
+				<h2 class="underlined">À PROPOS&nbsp;<span class="part1">&nbsp;DE MOI&nbsp;</span></h2>
+				<p>Je possède certaines compétences que vous pourrez prochainement découvrir <button class="link" href="#skills">ici</button> !</p>
+				<p>Vous pouvez également avoir un aperçu de mes <button data-name="experience" class="link" href="#experience">expériences professionelles</button></p>
+
+			</div>
+			<div id="skills">
+				<h2>Mes compétences</h2>
+				<p>il n'y a rien ici !</p>
+			</div>
+			<div id="experience" class="col s12">
+				<h2 class="underlined"><span class="part1">EXPÉ</span>RIENCES</h2>
+				<div>
+					<h4><span class="font-light">Développeur </span><span class="font-bold">Full-Stack</span> <button class="link_company" href="http://www.immo-facile.com/" target="_blank">@Immo Facile</button> - 2016 / 2017</h4>
+					<p>J'ai effectué une année en contrat de professionalisation chez Immo Facile en tant que développeur sur le logiciel de la société.</p>
+					<p>Au sein de l'équipe "Projets", j'ai pu participer à la réalisation de nouveaux modules et l'amélioration de l'existant.</p>
+					<p>J'ai également pu travailler seul sur un projet avec un partenaire, participé à la gestion de projet, et assuré le suivi post-production associée à celui-ci.</p>
+					<p>Ce projet a été l'objet d'un mémoire de fin d'année que vous pouvez téléchargez en <button class="link" href="#" >cliquant ici !</button></p>
+				</div>
+				<br />
+				<div class="right-align">
+					<h4><span class="font-bold">Vice </span><span class="font-light">Président</span> <button class="link_company" href="https://www.facebook.com/digitaldesign.brest/" target="_blank">@Digital Design</button> - 2016 / 2017</h4>
+					<p>Digital Design est une association étudiante de l'ISEN Brest spécialisée dans le design et les technologies orientée web.</p>
+					<p>Les élèves s'impliquent pour (au minimum) 2 années, la première avec une responsabilité moindre, pour s'habituer à ce genre d'activités.</p>
+					<p>La seconde année est destiné à prendre plus de responsabilité et transmettre son savoir aux autres élèves de l'association.</p>
+					<p>J'ai pour ma part été Responsable Communication avant d'évoluer au poste de Vice-Président. Cette année de vice-présidence a été particulièrement enrichissante puisque nous avons obtenu un partenariat avec une association sportive de Gouesnou (plus d'informations à venir à partir du 17 décembre !).</p>
+				</div>
+				<br />
+				<div>
+					<h4><span class="font-light">Stagiaire </span><span class="font-bold">Web</span> <button class="link_company" href="http://www.autocruise.com/societe-autocruise" target="_blank">@ZF - Autocruise</button> - 2016</h4>
+					<p>Stage de 7 semaines pendant lequel j'ai réalisé un projet de nouveau site web pour la société.</p>
+					<p>Pour ce projet, j'ai du réalisé un cahier des charges, faire les maquettes du site, effectuer la recherche de contenu et créer ce nouveau site web.</p>
+					<p>Étant donné le court laps de temps pour réaliser tout cela, le site web n'a pas pu être finalisé, et n'est donc pas en ligne.</p>
+					<p>Cependant, cela m'a permis d'appliquer dans la pratique, tout ce que j'avais pu apprendre de théorique.</p>
+				</div>
+			</div>
+			<div id="projets">
+				<h2>Mes projets</h2>
+				<p>il n'y a rien ici !</p>
+			</div>
+		</div>
+	</main>
+	<footer class="page-footer">
+		<div class="footer-copyright third">
+			<div class="copyright-left"><i class="fa fa-envelope-o" aria-hidden="true"></i> touin.marcantoine@gmail.com</div>
+			<div class="copyright-right"><a class="linkedin right" href="https://github.com/Extazx2" target="_blank">&nbsp;&nbsp;&nbsp;&nbsp;<i class="fa fa-github" aria-hidden="true"></i> Github</a><a class="linkedin right" href="https://www.linkedin.com/in/ma-touin//" target="_blank"><i class="fa fa-linkedin-square" aria-hidden="true"></i> Linkedin</a></div>
+		</div>
+		<div>
+			<div class="row">
+				<div class="col l0 s12 footer-middle">
+					<div class="copyright-left">
+					<h5>Bon à savoir</h5>
+					<p>Ce site est en cours de construction. Des modifications sont susceptibles d'être ajoutées fréquemment.
+						<br>Je vous suggère donc d'ajouter ce site à vos favoris et de venir y jeter un coup d'oeil de temps en temps !</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="footer-copyright">
+			<div class="copyright-left">© 2017 Marc-Antoine TOUIN</div>
+			<div class="copyright-right">Site hebergé chez OVH</div>
+		</div>
+	</footer>
+</body>
+</html>
--- js/script.js
+++ js/script.js
@@ -1,8 +1,34 @@
 document.addEventListener("DOMContentLoaded", function(event) {
-	console.log("toto");
-	//document.querySelectorAll('section').display = "none";
-	document.getElementById("projets").style.display = "none";
-	document.getElementById("skills").style.display = "none";
-	document.getElementById("experience").style.display = "none";
-	document.getElementById("contact").style.display = "none";
+	$("#projets").hide();
+	$("#skills").hide();
+	$("#experience").hide();
+	$("[data-name=index]").hide();
+
+	var btn_expe = document.querySelectorAll("button[data-name=experience]"),
+		btn_index = document.querySelectorAll("button[data-name=index],[class=logo]");
+	[].forEach.call(btn_expe, function(elem){
+		elem.addEventListener("click", launchExperience, false);
+	});
+	[].forEach.call(btn_index, function(elem){
+		elem.addEventListener("click", launchIndex, false);
+	});
+	//if you are lurking this, you should know that i'll upgrade this to react as soon as i can.
 });
+
+function launchExperience(){
+	var btn_expe = document.querySelector("[data-name=experience]");
+	var btn_index = document.querySelector("[data-name=index]");
+	$("#aboutme").hide(500);
+	$("#experience").show(500);
+	$(btn_expe).hide();
+	$(btn_index).show();
+}
+
+function launchIndex(){
+	var btn_expe = document.querySelector("[data-name=experience]");
+	var btn_index = document.querySelector("[data-name=index]");
+	$("#aboutme").show(500);
+	$("#experience").hide(500);
+	$(btn_expe).show();
+	$(btn_index).hide();
+}
