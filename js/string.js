063516f76b36daf073fdc86eabaf8d8c94373cf1
Extazx2 <touin.marcantoine@gmail.com>
2019-03-25 00:16:18 +0100

    update 2019 portfolio

---
+++ js/string.js
@@ -0,0 +1,44 @@
+var etudiant_ingenieur = "Étudiant ingénieur";
+var adresse = "16 Rue de la moutonnière";
+var ville_cp = "35130, La Guerche de Bretagne";
+var mail = "touin.marcantoine@gmail.com";
+var tel = "06.76.04.10.57";
+var age = "23 ans";
+var permis = "Permis B";
+var linkedin = "Mon LinkedIn";
+
+var titre_about = "À Propos de moi";
+var content_about = "Je suis Marc-Antoine Touin, étudiant en 5è année à l'ISEN Brest (Promotion 2019) et en contrat de professionnalisation à Niji.";
+
+var experiences = "Expériences";
+var poste_niji = "Ingénieur d'étude en contrat de professionnalisation - Niji Rennes - 2018 / 2019";
+var content_niji = "Réalisation d'un projet interne d'aide au recrutement pour tablettes en mobile, en Flutter.";
+var poste_osm = "Projet de génération de randonnées avec OpenStreetMap - ISEN - 2018";
+var content_osm = "Le but de ce projet est de proposer un outil permettant de générer une randonnée à partir d’un point de départ et de diverses options (distance / durée / point d’arrivée), grâce aux données OpenStreetMap.";
+var poste_ifacile = "Développeur en contrat de professionnalisation - Immo Facile - 2016 / 2017";
+var content_ifacile = "Travail au sein de l’équipe « projets » : réalisation de nouveaux modules, refonte de l’existant, travail avec partenaires et gestion de projet associée au sein du logiciel vendu par la société.";
+var poste_dd = "Vice-président et développeur - Digital Design - 2016 / 2017";
+var content_dd = "Réalisation de la communication de la 1è édition de <a href=\"http://www.laleclercgouesnou.fr/v2/\">La Leclerc Gouesnou Brest métropole.</a> <br>Digital Design est une association d'étudiant au sein de l'ISEN Brest orientée sur les technologies web et le design. <br>Auparavant responsable communication.";
+var poste_autocruise = "Développeur en stage de 7 semaines - Autocruise - 2016";
+var content_autocruise = "Au sein de l’équipe Qualité, réalisation d’un nouveau site web pour l’entreprise.";
+
+var formation = "Formation & Diplômes";
+var title_yncrea = "YNCREA ISEN Ouest, Brest - 2017 / Aujourd'hui";
+var content_yncrea = "Master 1 & 2 en option Génie Logiciel.";
+var title_isen = "ISEN Brest - 2013 / 2017";
+var content_isen = "Cycle préparatoire en Informatique & Réseaux au sein de l’ISEN.";
+var cisco = "CCNA Cisco 1,2,3,4";
+var fce = "First Certificate of English (FCE)";
+var voltaire = "Certification Voltaire";
+
+var skills = "Compétences";
+var title_prog = "Programmation";
+var content_prog = "";
+var title_concepts = "";
+var content_concepts = "";
+var title_tools = "";
+var content_tools = ""
+var title_lang = "Langues";
+var content_lang = "Français, Anglais (Compréhension & Expression), Espagnol (Compréhension et Expression), Italien (Compréhension)";
+var interest = "Centres d'intérêt & activités";
+var content_interest = "Football et sports en général, Séries TV, Musique, Cinéma, Cuisine, Jeux vidéo, Nouvelles technologies";
